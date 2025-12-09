8609f5b62c51828122351f863dd7076bb5c057fd
Lennon Strauss <koeideur@gmail.com>
2025-12-09 17:10:47 +0200

    fixed that input do not break up qoutes

--- src/main/java/net/broodguy/Main.java
+++ src/main/java/net/broodguy/Main.java
@@ -1,35 +1,44 @@
 package net.broodguy;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String input = "";
+        String input;
 
         while (true) {
             System.out.print("Task-Tracker-CLI ");
             input = sc.nextLine();
-            String[] parts = input.split(" ");
-            switch (parts[0]) {
+            List<String> parts = new ArrayList<>();
+            Matcher m = Pattern.compile("(\"[^\"]+\"|\\S+)").matcher(input);
+
+            while (m.find()) {
+                parts.add(m.group());
+            }
+            switch (parts.get(0)) {
                 case "add":
-                    Tasks.add(parts[1]);
+                    Tasks.add(parts.get(1));
                     break;
 
                 case "update":
-                    Tasks.updateDescription(Integer.parseInt(parts[1]), parts[2]);
+                    Tasks.updateDescription(Integer.parseInt(parts.get(1)), parts.get(2));
                     break;
 
                 case "delete":
-                    Tasks.delete(Integer.parseInt(parts[1]));
+                    Tasks.delete(Integer.parseInt(parts.get(1)));
                     break;
 
                 case "list":
-                    Tasks.list(parts[1]);
+                    Tasks.list(parts.get(1));
                     break;
 
                 case "mark":
-                    Tasks.updateStatus(Integer.parseInt(parts[1]), parts[2]);
+                    Tasks.updateStatus(Integer.parseInt(parts.get(1)), parts.get(2));
                     break;
 
                 case "exit":
